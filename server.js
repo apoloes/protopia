63aeda081a5940f403770dc8a63570a9f7a7c1fd
Joshua Shrestha <33375383+joshrestha7@users.noreply.github.com>
2019-11-22 18:02:56 -0500

    added open route to server

--- server.js
+++ server.js
@@ -2,37 +2,39 @@ const express = require('express');
 const request = require('request');
 const fs = require('fs')
 const path = require('path');
-var cors = require('cors');
-const {Storage} = require('@google-cloud/storage');
+const cors = require('cors');
+// const {Storage} = require('@google-cloud/storage');
 
 require('dotenv').config();
 
 const app = express();
 const PORT = 8000;
 const SERVER = process.env.SERVER;
-const allowedOrigins = ['http://localhost:8000', 'http://localhost:8080'];
+const SENDGRID_API_KEY = process.env.SENDGRID_API_KEY;
+const SENDGRID_URL = process.env.SENDGRID_URL;
+//const allowedOrigins = ['http://localhost:8000', 'http://localhost:8080'];
 
 app.use(express.static(path.join(__dirname, "/client")));
 
 app.use(cors({
-    origin: function(origin, callback){
-        if(!origin) return callback(null, true);
-        if(allowedOrigins.indexOf(origin) === -1){
-            var msg = 'The CORS policy for this site does not ' +
-                'allow access from the specified Origin.';
-            return callback(new Error(msg), false);
-        }
-        return callback(null, true);
-    }
+    // origin: function(origin, callback){
+    //     if(!origin) return callback(null, true);
+    //     if(allowedOrigins.indexOf(origin) === -1){
+    //         var msg = 'The CORS policy for this site does not ' +
+    //             'allow access from the specified Origin.';
+    //         return call back(new Error(msg), false);
+    //     }
+    //     return callback(null, true);
+    // }
 }));
 
 // Creates a client
-const storage = new Storage({
-    keyFilename: "./hermes-27600-2995a52d1b9a.json",
-    projectId: "hermes-27600"
-});
-
-const bucket = storage.bucket('protopia-comp523');
+// const storage = new Storage({
+//     keyFilename: "./hermes-27600-2995a52d1b9a.json",
+//     projectId: "hermes-27600"
+// });
+//
+// const bucket = storage.bucket('protopia-comp523');
 
 /* ---------- Routes ---------- */
 app.get('/api/asks', (req, res)=>{
@@ -43,37 +45,55 @@ app.get('/api/asks', (req, res)=>{
         if (error) {
             console.log(error);
         } else {
-            const jsonString = JSON.stringify(body);
-            fs.writeFile('./file.json', jsonString, err => {
-                if (err) {
-                    console.log('Error writing file', err)
-                } else {
-                    console.log('Successfully wrote file');
-                    res.send(body);
-                    // const filename = './file.json';
-                    //
-                    // bucket.upload(filename, {
-                    //     gzip: true,
-                    //     metadata: {
-                    //         cacheControl: 'no-cache',
-                    //     },
-                    // });
-                    //console.log(`${filename} uploaded to ` + bucket.);
-                }
-            });
-
+            res.send(body);
+            // const jsonString = JSON.stringify(body);
+            // fs.writeFile('./file.json', jsonString, err => {
+            //     if (err) {
+            //         console.log('Error writing file', err)
+            //     } else {
+            //         console.log('Successfully wrote file');
+            //         res.send(body);
+            //         const filename = './file.json';
+            //
+            //         bucket.upload(filename, {
+            //             gzip: true,
+            //             metadata: {
+            //                 cacheControl: 'no-cache',
+            //             },
+            //         });
+            //         console.log(`${filename} uploaded to ` + bucket.);
+            //     }
+            // });
         }
     });
 });
 
+app.get('/api/open', (req, res)=>{
+    request({
+        url: SENDGRID_URL,
+        headers: {
+            authorization: 'Bearer ' + SENDGRID_API_KEY
+        }
+    }, function(error, response, body) {
+        if (error) {console.log(error);}
+        else {
+            res.send(body);
+        }
+    })
+});
+
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + "/dist");
+    try {
+        res.sendFile(__dirname + "/dist");
+    } catch {
+        res.send("No build file available");
+    }
 });
 
 //Start server
 app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
-  console.log('Press Ctrl+C to quit.');
+    console.log(`App listening on port ${PORT}`);
+    console.log('Press Ctrl+C to quit.');
 });
 
 module.exports = app;
