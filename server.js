db8fb09d280acf6e83ff1fefcae9832e51010589
BenLu910 <benjamin.f.lu@gmail.com>
2019-12-08 14:41:14 -0500

    Revert "Merge branch 'master' of https://github.com/apoloes/protopia"
    This reverts commit 88e1a1d112805a17baea0b65c144bda507ff438f, reversing
    changes made to 2f4d8d5191348c408d523b551162e5cba7a274ee.

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = 8000;
+const SERVER = process.env.SERVER;
 const SENDGRID_API_KEY = process.env.SENDGRID_API_KEY;
 const SENDGRID_URL = process.env.SENDGRID_URL;
 //const allowedOrigins = ['http://localhost:8000', 'http://localhost:8080'];
@@ -82,7 +83,11 @@ app.get('/api/open', (req, res)=>{
 });
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + "/dist");
+    try {
+        res.sendFile(__dirname + "/dist");
+    } catch {
+        res.send("No build file available");
+    }
 });
 
 //Start server
